ab7e92b76346090b0d82aeb6167e2f1f990e86a9
Stanley Davies <stand@MacBookAir.lan>
2025-03-09 13:00:40 +0000

    Finished with translate.

--- translate/translate.c
+++ translate/translate.c
@@ -19,29 +19,19 @@ void translate_process(struct img *i) {
 }
 
 float transform_hue(float h) {
-        /*
-        float x = h / 360.f;
-        if (x < 0.3f) {
-                return 1.f - expf(2.f * x - 0.6f);
-        } else if (x > 0.7f) {
-                return 0.5f + expf(-2.f * x + 0.7f);
-        } else {
-                return powf(4.f * x - 2.f, 3.f) + 0.5f;
+        float nh = h / 360.f;
+        if (nh > 0.6282f) {
+                float t = 1.8f * sinhf(0.5f * nh);
+                return t * 360.f;
         }
-        */
-        /*
-        if (h / 360.f > 2.f / 3.f) {
-                return -0.1f * sinhf(2.f * h) + 0.39f;
-        }
-        float x = 2.4337f * (1.6f * (h / 360.f)) - 0.7f;
+        float x = 2.4337f * (1.6f * nh) - 0.7f;
         float f = -1.f * powf(x, 5.f); 
         f      +=  2.f * powf(x, 4.f);
         f      += 0.5f * powf(x, 3.f);
         f      += -2.f * powf(x, 2.f);
-        f      -= sinf(2.4337f * 1.6f * h);
+        f      -= sinf(2.4337f * 1.6f * nh);
         f      *= -0.8f;
         f      -= 0.3f;
-        return 1.05f - 0.5f * f + powf(h, 6.f);
-        */
-        return h;
+        float t = 1.05f - 0.5f * f + powf(nh, 6.f);
+        return t * 360.f;
 }
